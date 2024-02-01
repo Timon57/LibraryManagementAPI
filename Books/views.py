667e87e551c588dc1d96dc848c2231a692c6d485
Timon57 <timobasnet@gmail.com>
2024-02-01 22:51:44 +0545

    final update

--- Books/views.py
+++ Books/views.py
@@ -1,26 +1,54 @@
-from django.shortcuts import render,get_object_or_404
-from .models import *
-from .serializers import *
+from django.shortcuts import render, get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from django.db.models import ObjectDoesNotExist
+from .models import User, Book, BookDetails, BorrowedBooks
+from .serializers import UserSerializer, BookSerializer, BookDetailsSerializer, BorrowedBooksSerializer
 
+# User APIs
 
 @api_view(['GET'])
-def users_list(reqeust):
+def users_list(request):
+    """
+    Endpoint to retrieve a list of all users in the system.
+
+    Parameters:
+    - None
+
+    Returns:
+    - Response: List of user details.
+    """
     users = User.objects.all()
-    serializer = UserSerializer(users,many=True)
+    serializer = UserSerializer(users, many=True)
     return Response(serializer.data)
 
 @api_view(['GET'])
 def get_user(request, user_id):
+    """
+    Endpoint to fetch a user's details using their UserID.
+
+    Parameters:
+    - user_id (int): ID of the user.
+
+    Returns:
+    - Response: User details.
+    """
     user = get_object_or_404(User, UserID=user_id)
     serializer = UserSerializer(user)
     return Response(serializer.data)
 
 @api_view(['POST'])
 def create_user(request):
+    """
+    Endpoint to add a new user to the system with details like name, email, and membership date.
+
+    Parameters:
+    - request.data: User details including name, email, and membership date.
+
+    Returns:
+    - Response: Created user details.
+    """
     serializer = UserSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
@@ -28,8 +56,18 @@ def create_user(request):
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 # Book APIs
+
 @api_view(['POST'])
 def add_new_book(request):
+    """
+    Endpoint to add a new book record, including title, ISBN, published date, and genre.
+
+    Parameters:
+    - request.data: Book details including title, ISBN, published date, and genre.
+
+    Returns:
+    - Response: Created book details.
+    """
     serializer = BookSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
@@ -38,20 +76,46 @@ def add_new_book(request):
 
 @api_view(['GET'])
 def list_all_books(request):
+    """
+    Endpoint to retrieve a list of all books in the library.
+
+    Parameters:
+    - None
+
+    Returns:
+    - Response: List of book details.
+    """
     books = Book.objects.all()
     serializer = BookSerializer(books, many=True)
     return Response(serializer.data)
 
 @api_view(['GET'])
 def get_book(request, book_id):
+    """
+    Endpoint to fetch details of a specific book using its BookID.
+
+    Parameters:
+    - book_id (int): ID of the book.
+
+    Returns:
+    - Response: Book details.
+    """
     book = get_object_or_404(Book, BookID=book_id)
     serializer = BookSerializer(book)
     return Response(serializer.data)
 
-
-
 @api_view(['PUT'])
 def update_book_details(request, book_id):
+    """
+    Endpoint to assign details to a book or update existing book details, like the number of pages, publisher, language.
+
+    Parameters:
+    - book_id (int): ID of the book.
+    - request.data: Book details including number of pages, publisher, language.
+
+    Returns:
+    - Response: Updated book details.
+    """
     book = get_object_or_404(Book, BookID=book_id)
 
     # Try to get the existing BookDetails or create a new one
@@ -65,11 +129,19 @@ def update_book_details(request, book_id):
     
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
-
 # BorrowedBooks APIs
+
 @api_view(['POST'])
 def borrow_book(request):
+    """
+    Endpoint to record the borrowing of a book by linking a user with a book.
+
+    Parameters:
+    - request.data: Borrowed book details including UserID, BookID, and BorrowDate.
+
+    Returns:
+    - Response: Created borrowed book details.
+    """
     serializer = BorrowedBooksSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
@@ -78,6 +150,16 @@ def borrow_book(request):
 
 @api_view(['PUT'])
 def return_book(request, borrowed_book_id):
+    """
+    Endpoint to update the system when a book is returned.
+
+    Parameters:
+    - borrowed_book_id (int): ID of the borrowed book.
+    - request.data: Return details including return_date.
+
+    Returns:
+    - Response: Success message.
+    """
     borrowed_book = get_object_or_404(BorrowedBooks, id=borrowed_book_id)
     borrowed_book.return_date = request.data.get('return_date')
     borrowed_book.save()
@@ -85,6 +167,15 @@ def return_book(request, borrowed_book_id):
 
 @api_view(['GET'])
 def list_all_borrowed_books(request):
+    """
+    Endpoint to list all books currently borrowed from the library.
+
+    Parameters:
+    - None
+
+    Returns:
+    - Response: List of borrowed book details.
+    """
     borrowed_books = BorrowedBooks.objects.all()
     serializer = BorrowedBooksSerializer(borrowed_books, many=True)
     return Response(serializer.data)
--- README.md
+++ README.md
@@ -1,40 +1,41 @@
 # LibraryManagementAPI
 Task work for building library Management System
-Download & Setup Instructions :
-Before downloading the project, check to make sure you meet the project's requirements.
+* Download & Setup Instructions :
+* Before downloading the project, check to make sure you meet the project's requirements.
 
-Clone the project. This will download the GitHub respository files onto your local machine.
+* Clone the project. This will download the GitHub respository files onto your local machine.
 
 git clone https://github.com/divanov11/mumble
-Install Dependencies:
+
+* Install Dependencies:
 pip install -r requirements.txt
-Database Migrations:
+* Database Migrations:
 python manage.py makemigrations
 python manage.py migrate
-Run the Development Server:
+* Run the Development Server:
 python manage.py runserver
 
 
-API Documentation
+## API Documentation
 
-Create a New User:
+* 1.Create a New User:
 users/create/
-List All Users:
+* 2.List All Users:
 users/
-Get User by ID:
-users/<int:user_id>/
-Add a New Book:
+* 3.Get User by ID:
+* users/<int:user_id>/
+* 4.Add a New Book:
 books/add/
-List All Books:
+* 5.List All Books:
 books/
-Get Book by ID:
+* 6.Get Book by ID:
 books/<int:book_id>/
-Assign/Update Book Details:
+* 7.Assign/Update Book Details:
 books/<int:book_id>/update-details/
-Borrow a Book:
+* 8.Borrow a Book:
 books/borrow/
-Return a Book: 
+* 9.Return a Book: 
 books/return/<int:borrowed_book_id>/
-List All Borrowed Books:
+* 10.List All Borrowed Books:
 books/borrowed/
 
